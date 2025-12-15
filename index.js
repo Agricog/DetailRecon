a3539329cee2a90ef47ea168f6bf5d144873f5a8
Agricog <mick@mickstevenson.com>
2025-12-15 19:15:11 +0000

    Update index.js

--- index.js
+++ index.js
@@ -2,30 +2,65 @@ const http = require('http');
 const fs = require('fs');
 const path = require('path');
 
+const mimeTypes = {
+  '.html': 'text/html',
+  '.css': 'text/css',
+  '.js': 'application/javascript',
+  '.json': 'application/json',
+  '.png': 'image/png',
+  '.jpg': 'image/jpeg',
+  '.jpeg': 'image/jpeg',
+  '.gif': 'image/gif',
+  '.svg': 'image/svg+xml',
+  '.pdf': 'application/pdf',
+  '.xml': 'application/xml',
+  '.txt': 'text/plain',
+  '.ico': 'image/x-icon'
+};
+
 const server = http.createServer((req, res) => {
-  // Normalize URL (remove query strings)
   let url = req.url.split('?')[0];
-  
-  // Map routes to files
   let filePath;
+  
   if (url === '/' || url === '') {
     filePath = path.join(__dirname, 'index.html');
   } else if (url === '/privacy') {
     filePath = path.join(__dirname, 'privacy.html');
   } else if (url === '/terms') {
     filePath = path.join(__dirname, 'terms.html');
+  } else if (url === '/demo') {
+    filePath = path.join(__dirname, 'demo.html');
+  } else if (url === '/sample-report.pdf') {
+    filePath = path.join(__dirname, 'DetailRecon_Demo_Report.pdf');
+  } else if (url === '/sitemap.xml') {
+    filePath = path.join(__dirname, 'public', 'sitemap.xml');
+  } else if (url === '/robots.txt') {
+    filePath = path.join(__dirname, 'robots.txt');
   } else {
-    filePath = path.join(__dirname, url);
+    filePath = path.join(__dirname, 'public', url);
+    if (!fs.existsSync(filePath)) {
+      filePath = path.join(__dirname, url);
+    }
   }
   
-  fs.readFile(filePath, 'utf8', (err, data) => {
+  const ext = path.extname(filePath).toLowerCase();
+  const contentType = mimeTypes[ext] || 'application/octet-stream';
+  const isBinary = ['.pdf', '.png', '.jpg', '.jpeg', '.gif', '.ico'].includes(ext);
+  
+  fs.readFile(filePath, isBinary ? null : 'utf8', (err, data) => {
     if (err) {
       res.writeHead(404, { 'Content-Type': 'text/html' });
       res.end('<h1>404 - Page Not Found</h1>');
       return;
     }
     
-    res.writeHead(200, { 'Content-Type': 'text/html' });
+    const headers = { 'Content-Type': contentType };
+    if (ext === '.pdf') {
+      headers['Content-Disposition'] = 'attachment; filename="DetailRecon_Sample_Report.pdf"';
+      headers['Content-Length'] = data.length;
+    }
+    
+    res.writeHead(200, headers);
     res.end(data);
   });
 });
